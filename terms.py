cdebddde0deb3bc825d67d925a2cd09fcfaeeb9a
Darrell Jenkins <darrellajenkins@gmail.com>
2025-06-25 00:09:15 -0700

    Added terms module containing dict of industry terminology.

---
+++ terms.py
@@ -0,0 +1,29 @@
+
+# A comprehensive industry terms dictionary for use by employees. Access via gui database interface.
+
+ice_cream_dict = {
+	"Affogato al caffe": "A shot of espresso poured over a scoop of gelato.",
+	"A la mode": "A dessert served with ice cream, usually on top.",
+	"Awful Awful": "Flavored syrup, milk, and a frozen-ice-milk blend that turns out much like a milkshake.",
+	"Balboa Bar": "A rectangle-shaped vanilla ice cream on a stick, dipped in chocolate and coated in toppings like nuts and sprinkles.",
+	"Banana split": "A sundae made with a banana sliced lengthwise, three scoops of ice cream, and toppings such as: whipped cream, "
+                     "pineapple or cherry sauce, fudge, caramel, peanuts, and a cherry on top. The dessert was invented in 1904 by an optometrist"
+                     "named David 'Doc' Stickler in Latrobe, Pennsylvania.",
+	"Baskin-Robbins": "The world's largest chain of ice cream shops was founded in 1945 by Burt Baskin and his brother-in-law Irv Robbins in "
+	                  "Glendale, California. Famous for serving 31 flavors, the chain operates over 7,600 shops in nearly 50 countries and is "
+	                  "operated by Dunkin’ brands.",
+	"Ben & Jerry's": "Ben Cohen and Jerry Greenfield founded the ice cream company in 1978 in Burlington, Vermont. As of August 3, 2000 Ben & Jerry's "
+	                 "became a wholly-owned subsidiary of Unilever.",
+	"Bomb": "Ice cream molded into a single-serving sized sphere ical or half spherical shape. Covered with a glaze ior chocolate.",
+	"Bomb Pop": "Bomb Pops were invented in 1955 by James S. Merritt and D.S. Abernethy in Kansas City, Missouri. They are ice pops divided into three sections"
+	            "made with cherry, lime, and blue raspberry, giving it the famous red, white, and blue colors.",
+	"Breyers": "A delicious ice cream made with all natural ingredients. Started in 1866 by William Breyer who sold ice cream on the street with his horse"
+                "and buggy. Now owned by Unilever.",
+	"Coffee Cabinet": "A coffee cabinet is an ice cream-based milkshake found almost exclusively in Rhode Island consisting of coffee ice cream, coffee syrup, "
+                       "and milk. The ingredients are mixed in a drink blender or milkshake blender.",
+	"Carvel": "Carvel is an American ice cream franchise owned by Focus Brands, best known for its soft-serve ice cream and ice cream cakes, featuring a layer "
+	          "of crunchies made with cookie pieces.",
+	"Choco Taco": "Invented in Philadelphia by Alan Drazen in 1983.  It contained ice cream inside a waffle cone in the shape of a hard taco shell. Topped"
+	              "with fudge, peanuts, and milk chocolate.  This product was discontinued by Unilever in 2022."
+
+}
